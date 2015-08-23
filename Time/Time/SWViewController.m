5c376f893255b35fe2d7dc85af490bcd853c3f20
cdolm92 <cdolm92@gmail.com>
2015-08-23 15:02:44 -0400

    work on stopwatch label

--- Time/Time/SWViewController.m
+++ Time/Time/SWViewController.m
@@ -10,13 +10,18 @@
 
 @interface SWViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *SWlabel;
+
+@property (strong, nonatomic) NSTimer *stopWatchTimer; // Store the timer that fires after a certain time
+@property (strong, nonatomic) NSDate *startDate; // Stores the date of the click on the start button
+
 @end
 
 @implementation SWViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+     self.SWlabel.text = @"00:00:00";
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,8 +29,47 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)updateTimer
+{
+    static NSInteger counter = 0;
+    self.SWlabel.text = [NSString stringWithFormat:@"Counter: %li", (long)counter++];
+}
+
+
+
+
+
+- (IBAction)startTimer:(id)sender {
+   
+    self.startDate = [NSDate date];
+    
+    // Create the stop watch timer that fires every 100 ms
+    self.stopWatchTimer = [NSTimer scheduledTimerWithTimeInterval:1.0/10.0
+            target:self
+          selector:@selector(updateTimer)
+          userInfo:nil
+           repeats:YES];
+}
+
+- (IBAction)pauseTimer:(id)sender {
+    self.SWlabel.text=@"stop pressed";
+    
+}
+
+- (void)timerFired:(NSTimer *)timer {
+    
+}
+
+
+
+
+
+
+
+
+
 /*
-#pragma mark - Navigation
+#pragma mark - Navigation   `
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
