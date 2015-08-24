188f483ac4e0885eb474ef42ac742fc163553bac
ayunav <ayunanyc@gmail.com>
2015-08-24 18:42:03 -0400

    added properties for label, date picker view, and buttons in the Timer VC.m file from the main.storyboard.

---
+++ Time/Time/CQPresetTimerViewController.h
@@ -0,0 +1,13 @@
+//
+//  CQPresetTimerViewController.h
+//  Time
+//
+//  Created by Ayuna Vogel on 8/24/15.
+//  Copyright (c) 2015 Mike Kavouras. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CQPresetTimerViewController : UIViewController
+
+@end
