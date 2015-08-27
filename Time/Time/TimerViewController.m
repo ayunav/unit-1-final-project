5a25b07b82f850df7626362cc94be50234ea4848
ayunav <ayunanyc@gmail.com>
2015-08-27 17:54:41 -0400

    added code for NSTimer, viewDidLoad method, timerFired, and buttons Start, Stop, Pause, Resume. Fixed UI so the timer label displays properly.

--- Time/Time/TimerViewController.m
+++ Time/Time/TimerViewController.m
@@ -10,15 +10,22 @@
 
 @interface TimerViewController ()
 
-@property (nonatomic) NSMutableArray *presetTimers;
-@property (weak, nonatomic) IBOutlet UITableView *timersTableView;
+@property (nonatomic) NSTimer *timer;
+//@property (nonatomic) NSTimeInterval timerDuration;
 @property (weak, nonatomic) IBOutlet UIDatePicker *timerPickerView;
 @property (weak, nonatomic) IBOutlet UILabel *timerLabel;
 @property (weak, nonatomic) IBOutlet UIButton *startButtonTapped;
 @property (weak, nonatomic) IBOutlet UIButton *pauseButtonTapped;
+@property (weak, nonatomic) IBOutlet UITableView *timersTableView;
+@property (nonatomic) NSMutableArray *presetTimers;
 
-@property (nonatomic) NSTimer *timer;
-@property (nonatomic) NSTimeInterval timerDuration;
+@property (nonatomic) BOOL isTimerRunning;
+@property (nonatomic) BOOL isTimerPaused;
+
+@property (nonatomic) int secondsCount;
+@property (nonatomic) int hours;
+@property (nonatomic) int minutes;
+@property (nonatomic) int seconds;
 
 @end
 
@@ -26,26 +33,111 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    //control dragging from tableview in the storyboard to the timer itself and setting it to its datasource and delegates implements the code below
-//    self.timersTableView.dataSource = self;
-//    self.timersTableView.delegate = self;
     
-//do I need the line of code below?
-    //    [self.timersTableView reloadData];
-    
+    self.isTimerRunning = NO;
+    self.isTimerPaused = NO;
+    self.pauseButtonTapped.enabled = NO;
     [self.timerLabel setHidden:YES];
 
+    NSInteger seconds = 60;
+    [self.timerPickerView setDatePickerMode:UIDatePickerModeCountDownTimer];
+    [self.timerPickerView setCountDownDuration:seconds];
+    
+    //tableview methods
     if (!self.presetTimers) {
         self.presetTimers = [[NSMutableArray alloc] init];
     }
     [self.presetTimers addObject:@"Coffee timer"];
     [self.presetTimers addObject:@"Popcorn timer"];
+
+    //control dragging from tableview in the storyboard to the timer itself and setting it to its datasource and delegates implements the code below
+    //    self.timersTableView.dataSource = self;
+    //    self.timersTableView.delegate = self;
     
-    NSInteger seconds = 60;
-    //[self.timerPickerView setDatePickerMode:UIDatePickerModeCountDownTimer];
-    [self.timerPickerView setCountDownDuration:seconds];
+    //do I need the line of code below?
+    //    [self.timersTableView reloadData];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+- (void)timerFired: (NSTimer *)timer {
+    self.secondsCount--;
     
+    self.hours = self.secondsCount/3600;
+    self.minutes = (self.secondsCount % 3600)/60;
+    self.seconds = self.secondsCount - (self.hours * 3600) - (self.minutes * 60);
+    NSLog(@"%d, %d, %d", self.hours, self.minutes, self.seconds);
     
+    self.timerLabel.text = [NSString stringWithFormat:@"%02i:%02i:%02i", self.hours, self.minutes, self.seconds];
+    self.timerLabel.hidden = NO;
+    self.timerPickerView.hidden = YES;
+    
+    if (self.secondsCount <= 0) {
+        
+        [self.timer invalidate];
+        self.timer = nil;
+        
+        [self.startButtonTapped setTitle:@"Start" forState:UIControlStateNormal];
+        self.pauseButtonTapped.enabled = NO;
+        
+    }
+}
+
+#pragma mark - Button methods
+//start & stop Button Tapped 
+- (IBAction)startButtonTapped:(id)sender {
+    NSTimeInterval duration = self.timerPickerView.countDownDuration;
+    
+    self.seconds = 0;
+    self.hours = (int)(duration/3600.0f);
+    self.minutes = ((int)duration - (self.hours * 3600))/60;
+    
+    self.secondsCount = ((self.hours * 3600) + (self.minutes * 60));
+    
+    self.timerLabel.text = [NSString stringWithFormat:@"%02i:%02i:%02i", self.hours, self.minutes, self.seconds];
+    
+    if (self.isTimerRunning == YES) {
+        
+        [self.startButtonTapped setTitle:@"Start" forState:UIControlStateNormal];
+        [self.pauseButtonTapped setTitle:@"Pause" forState:UIControlStateNormal];
+        self.pauseButtonTapped.enabled = NO;
+        
+        [self.timer invalidate];
+        self.timer = nil;
+        
+    } else {
+        
+        [self.startButtonTapped setTitle:@"Stop" forState:UIControlStateNormal];
+        self.pauseButtonTapped.enabled = YES;
+        
+        if (self.timer == nil) {
+            self.timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(timerFired:) userInfo:nil repeats:YES];
+        }
+    }
+    
+    self.isTimerRunning = !self.isTimerRunning;
+}
+
+//pause & resume Button Tapped
+- (IBAction)pauseButtonTapped:(id)sender {
+    if (self.isTimerPaused == NO) {
+        [self.timer invalidate];
+        self.timer = nil;
+        
+        [self.pauseButtonTapped setTitle:@"Resume" forState:UIControlStateNormal];
+        
+    } else {
+        if (self.timer) {
+            [self.timer invalidate];
+            self.timer = nil;
+        }
+        self.timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(timerFired:) userInfo:nil repeats:YES];
+        [self.pauseButtonTapped setTitle:@"Pause" forState:UIControlStateNormal];
+    }
+    
+    self.isTimerPaused = !self.isTimerPaused;
 }
 
 #pragma mark - Table View methods
@@ -63,54 +155,6 @@
     cell.textLabel.text = [self.presetTimers objectAtIndex:indexPath.row];
     return cell;
 }
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-#pragma mark - Button methods 
-
-- (IBAction)startButtonTapped:(id)sender {
-    if(sender == self.startButtonTapped){
-        [self.timerLabel setHidden:NO];
-        [self.timerPickerView setHidden:YES];
-        [sender setTitle:@"Stop" forState: UIControlStateNormal];
-        
-        NSTimer *timer = [NSTimer timerWithTimeInterval:1.0 target:self selector:@selector(timerFired:) userInfo:nil repeats:YES];
-        [[NSRunLoop currentRunLoop] addTimer:timer forMode:NSRunLoopCommonModes];
-        self.timerDuration = self.timerPickerView.countDownDuration;
-    }
-}
-
-- (void)timerFired: (NSTimer *)timer {
-    self.timerDuration = self.timerDuration - 1;
-    NSInteger secondsCount = self.timerDuration;
-    NSInteger minutes = secondsCount / 60;
-    NSInteger seconds = secondsCount - (minutes * 60);
-    NSString *outputTimer = [NSString stringWithFormat:@"%02lu:%02lu", minutes, seconds];
-    
-    self.timerLabel.text = outputTimer;
-    
-//    if (nextNumber == 10) {
-        //        [timer invalidate];
-        //    }
-
-    
-    
-    
-    //code from the exercise in class
-//    NSInteger currentNumber = [self.timerLabel.text integerValue];
-//    NSInteger nextNumber = currentNumber + 1;
-//    
-//    self.timerLabel.text = [NSString stringWithFormat:@"%lu", nextNumber];
-//    
-//    if (nextNumber == 10) {
-//        [timer invalidate];
-//    }
-//    NSLog(@"Timer fired");
-}
-
 
 /*
 #pragma mark - Navigation
