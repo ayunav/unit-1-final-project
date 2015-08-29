3e10555670ea21ee6040ce75aa0a828c08e35a67
ayunav <ayunanyc@gmail.com>
2015-08-29 10:25:06 -0400

    Added files for CQTimer class and some properties to it.

---
+++ Time/Time/CQTimer.m
@@ -0,0 +1,13 @@
+//
+//  CQTimer.m
+//  Time
+//
+//  Created by Ayuna Vogel on 8/28/15.
+//  Copyright (c) 2015 Mike Kavouras. All rights reserved.
+//
+
+#import "CQTimer.h"
+
+@implementation CQTimer
+
+@end
