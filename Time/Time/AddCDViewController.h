ccc4458829be1a8565c029a91a6c3cfd3b7f8d35
cdolm92 <cdolm92@gmail.com>
2015-09-01 16:23:22 -0400

    added the view files

---
+++ Time/Time/AddCDViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddCDViewController.h
+//  Time
+//
+//  Created by Christella on 9/1/15.
+//  Copyright (c) 2015 Mike Kavouras. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddCDViewController : UIViewController
+
+@end
---
+++ Time/Time/ShowCDViewController.h
@@ -0,0 +1,13 @@
+//
+//  ShowCDViewController.h
+//  Time
+//
+//  Created by Christella on 9/1/15.
+//  Copyright (c) 2015 Mike Kavouras. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ShowCDViewController : UIViewController
+
+@end
